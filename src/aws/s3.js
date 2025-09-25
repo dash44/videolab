cda876133fd9c522a91dd171ab65d345d64d3b96
dash44 <d.naumova@connect.qut.edu.au>
2025-09-25 16:53:14 +1000

    updates to s3

--- src/aws/s3.js
+++ src/aws/s3.js
@@ -4,12 +4,25 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 const region = process.env.AWS_REGION || "ap-southeast-2";
 export const s3 = new S3Client({ region });
 
-export async function presignPut(bucket, key, expiresSeconds = 900) {
-    const cmd = new PutObjectCommand({ Bucker: bucket, Key: key });
+export async function presignPut({ bucket, key, expiresSeconds = 900 }) {
+    const cmd = new PutObjectCommand({ 
+        Bucket: bucket, 
+        Key: key,
+        ...(contentType ? { ContentType: contentType } : {}) 
+    });
     return getSignedUrl(s3, cmd, { expiresIn: expiresSeconds });
 }
 
-export async function presignGet(bucket, key, expiresSeconds = 900) {
-    const cmd = new GetObjectCommand({ Bucket: bucket, Key: key });
+export async function presignGet({ 
+    bucket, 
+    key, 
+    expiresSeconds = 900,
+    responseContentDisposition 
+}) {
+    const cmd = new GetObjectCommand({ 
+        Bucket: bucket, 
+        Key: key,
+        ...(responseContentDisposition ? { ResponseContentDisposition: responseContentDisposition } : {}) 
+    });
     return getSignedUrl(s3, cmd, { expiresIn: expiresSeconds });
 }
