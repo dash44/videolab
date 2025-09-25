92e1f8face7be5b6582056e4621a4c3759b68afc
dash44 <d.naumova@connect.qut.edu.au>
2025-09-25 16:53:02 +1000

    routes wrapper

---
+++ src/services/storage.js
@@ -0,0 +1,20 @@
+import { presignPut, presignGet } from "../aws/s3";
+
+/**
+ * IMPORTANT: client must use same Content Type header used here
+ */
+
+export async function createUploadUrl({ bucket, key, contentType, expiresSeconds = 900}) {
+    if (!bucket || !key) throw new Error("bucket and key required!!!!");
+    return presignPut({ bucket, key, contentType, expiresSeconds });
+}
+
+export async function createDownloadUrl({ bucket, key, expiresSeconds = 900, asAttachmentName }) {
+    if (!bucket || !key) throw new Error("bucket and key required!!!!");
+    return presignGet({ 
+        bucket, 
+        key, 
+        expiresSeconds,
+        responseContentDisposition: asAttachmentName ? `attachment; filename="${asAttachmentName}"` : undefined
+     });
+}
